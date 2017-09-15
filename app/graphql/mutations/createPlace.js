9030edb663e701d49f3efd3d7d22010eb33d3558
IlyaSuhodolskiy <ilya.suhodolskiy@gmail.com>
2017-09-15 15:11:59 +0300

    Add mutation createPlace

---
+++ app/graphql/mutations/createPlace.js
@@ -0,0 +1,33 @@
+import { GraphQLNonNull, GraphQLID, GraphQLString, GraphQLFloat, GraphQLInt } from 'graphql';
+
+import PlaceModel from '../../models/PlaceModel';
+import PlaceType from '../types/PlaceType';
+
+import { authUser } from '../../lib/permissions';
+
+export default {
+  type: PlaceType,
+  description: 'Create place',
+  args: {
+    name: {
+      type: new GraphQLNonNull(GraphQLID)
+    },
+    addr: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'Full address place'
+    },
+    coordinates: {
+      type: new GraphQLList(GraphQLFloat),
+      description: 'Coordinates of place: latitude, longitude'
+    },
+    price: {
+      type: GraphQLInt,
+      defaultValue: 1
+    },
+    categories: {
+      type: new GraphQLList(GraphQLID)
+    },
+  },
+  resolve: authUser(async (root, { coordinates, ...data }, { userId }) =>
+    await PlaceModel.create({ ...data, loc: { coordinates }, creator: userId }))
+}
--- app/graphql/mutations/index.js
+++ app/graphql/mutations/index.js
@@ -6,3 +6,6 @@ export { default as createClientApp } from './createClientApp';
 // Auth
 export { default as authorization } from './authorization';
 export { default as refreshTokens } from './refreshTokens';
+
+// Place
+export { default as createPlace } from './createPlace';
